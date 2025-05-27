549e59a301234275478f3181f0dae49c399d12d9
Henna Parveen <hparveen@lab1r5s12.42abudhabi.ae>
2025-05-27 12:44:41 +0400

    seg fault fixed

--- minishell/initialise.c
+++ minishell/initialise.c
@@ -6,7 +6,7 @@
 /*   By: hparveen <hparveen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 13:10:19 by hparveen          #+#    #+#             */
-/*   Updated: 2025/05/24 15:56:50 by hparveen         ###   ########.fr       */
+/*   Updated: 2025/05/27 12:43:54 by hparveen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,6 +126,7 @@ void	init(t_shell *shell, char **envp)
 	shell->prompt = NULL;
 	shell->env_array = create_env_array(envp);
 	shell->env_list = NULL;
+	shell->token_list = NULL;
 	shell->index = 0;
 	if (!shell->env_array)
 	{
