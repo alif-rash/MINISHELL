d1f7301442d3cf8ebc103cffc6e483491e779bca
hennaparveen <hennaparveen77@gmail.com>
2025-06-24 11:41:55 +0400

    removed scripted executing in binary

--- execution/execute_external.c
+++ execution/execute_external.c
@@ -6,7 +6,7 @@
 /*   By: hparveen <hparveen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 19:34:12 by hparveen          #+#    #+#             */
-/*   Updated: 2025/06/23 08:31:35 by hparveen         ###   ########.fr       */
+/*   Updated: 2025/06/24 11:41:08 by hparveen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,8 +72,6 @@ static int	run_binary(t_shell *shell, char *command, char **args)
 		return (handle_exec_errors(shell, exec_path, args[0], path_found));
 	update_env_array(shell, shell->env_list, ft_envlist_size(shell->env_list));
 	execve(exec_path, args, shell->env_array);
-	if (errno == ENOEXEC)
-		return (handle_noexec(shell, exec_path, args[0]));
 	if (exec_path && !stat(exec_path, &file_info) && S_ISDIR(file_info.st_mode))
 		return (handle_error(shell, exec_path, ERROR_GENERIC, IS_DIR), 126);
 	return (1);
--- parsing/check_syntax.c
+++ parsing/check_syntax.c
@@ -6,7 +6,7 @@
 /*   By: hparveen <hparveen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 10:49:04 by hparveen          #+#    #+#             */
-/*   Updated: 2025/06/24 11:05:39 by hparveen         ###   ########.fr       */
+/*   Updated: 2025/06/24 11:38:15 by hparveen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ int	syntax_check_redirections(t_token *current)
 	if (current->type != T_HEREDOC && current->next->type != T_FILENAME)
 		return (ERR_REDIR_SYNTAX);
 	if (current->next && current->next->type == T_FILENAME
-		&& ft_strchr(current->next->value, '*'))
+		&& current->next->value[0] == '*' && current->next->value[1] == '\0')
 		return (ERR_AMBIGOUS_REDIRECT);
 	return (0);
 }
